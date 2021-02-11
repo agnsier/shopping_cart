51934544b6eda2cc263e4414f85261b433ee28a2
Agnieszka <agn.sierocinska@gmail.com>
2021-02-12 00:20:59 +0100

    redux setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../sass/App.scss';
-import ProductsList from "../Products/ProductsList";
-
+import {useDispatch, useSelector} from "react-redux";
+import {getProductsRequest} from "../../redux/actions"
+import ProductCounter from "../Products/ProductCounter";
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getProductsRequest())
+    }, [])
+    const products = useSelector(state => state.products)
+    if(products.loading){
+        return "Loading..."
+    }
     return (
         <div className="container">
-            <ProductsList/>
+            <div className="products">
+                <h3 className="products__title">Lista produktów</h3>
+                <ul className="products__list">
+                    {products && products.list && products.list.length > 0 ? (
+                        products.list.map((product) => (
+                            <li className="products__row" key={product.pid}>
+                                <span>{product.name}</span>
+                                <span>{`${product.price} zł`}</span>
+                                <ProductCounter/>
+                            </li>
+                        ))
+                    ):(
+                        <div>Placeholder</div>
+                    )
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
